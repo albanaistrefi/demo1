df8929ebfa84732985a65b7c8d88dbd8cf8efdcf
Albana Istrefaj <albana.istrefaj@sjsu.edu>
2022-04-05 10:35:18 +0200

    added something

--- src/main/java/ch/hslu/sweng/App.java
+++ src/main/java/ch/hslu/sweng/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        System.out.println( "Hello Class!" );
+        System.out.println( "Hello !" );
         System.out.println( "Hello Class!" );
     }
 }
